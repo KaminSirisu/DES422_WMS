11d5d0c2fca6f7cf359776b2d5dcce1c364cdd58
Natthawxt1 <natthawxt1@gmail.com>
2026-04-19 17:08:06 +0700

    fix auth,admin middleware and inbound.rountes

--- backend/src/routes/inbound.routes.js
+++ backend/src/routes/inbound.routes.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 const { addStock } = require("../controllers/inbound.controller");
-const { authMiddleware } = require("../middleware/auth.middleware");
-const { isAdmin } = require("../middleware/admin.middleware");
+const authMiddleware = require("../middleware/auth.middleware");
+const isAdmin = require("../middleware/admin.middleware");
 
 router.post("/", authMiddleware, isAdmin, addStock);
 
